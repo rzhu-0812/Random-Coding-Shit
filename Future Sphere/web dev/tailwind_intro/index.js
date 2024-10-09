f251f78a2ff79459273961bf2689f9b0f83beaf3
rzhu-0812 <86477098+rzhu-0812@users.noreply.github.com>
2024-10-09 12:05:09 -0400

    started js

---
+++ Future Sphere/web dev/tailwind_intro/index.js
@@ -0,0 +1,16 @@
+console.log("Hello World")
+
+let user = "Ryan", age = 16, message = "Hello";
+
+// alert(message + " "  + user + "! You are " + age + " years old.")
+
+let $ = 4294967296;
+let _ = 4294967296 / 2;
+
+// alert($ + _)
+
+let name, admin;
+name = "john";
+admin = name
+alert(admin + " is the admin")
+
